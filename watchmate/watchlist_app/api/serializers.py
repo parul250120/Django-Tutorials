3c3ba5efb6fc434a53f2363ccf20670df895d5f3
parul250120 <paruljain.it22@gmail.com>
2022-08-02 07:18:22 +0530

    Validations

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -3,9 +3,14 @@ from rest_framework import serializers
 from watchlist_app.models import Movie
 
 
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("The name is to short!")
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -18,3 +23,17 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    # object level validation
+    def validate(self, data):
+        if data["name"] == data["description"]:
+            raise serializers.ValidationError("Name and description should be different!")
+        else:
+            return data
+
+    # field level validation
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("The name is to short!")
+    #     else:
+    #         return value
