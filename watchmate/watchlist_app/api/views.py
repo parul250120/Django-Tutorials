ad2fa1759b2fe6e9e84f893ca90471dfba3aa532
parul250120 <paruljain.it22@gmail.com>
2022-08-04 21:02:28 +0530

    User model - Temporary login and logout

--- watchmate/watchlist_app/api/views.py
+++ watchmate/watchlist_app/api/views.py
@@ -1,5 +1,6 @@
 # from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
+from rest_framework.exceptions import ValidationError
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, mixins, generics, viewsets
@@ -10,11 +11,20 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 class ReviewCreate(generics.CreateAPIView):
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs.get('pk')
         watchlist = WatchList.objects.get(pk=pk)
 
-        serializer.save(watchlist=watchlist)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=watchlist,
+                                                review_user=review_user)
+        if review_queryset.exists():
+            raise ValidationError("You have already reviewed this movie")
+
+        serializer.save(watchlist=watchlist, review_user=review_user)
 
 
 class ReviewList(generics.ListAPIView):
--- watchmate/watchmate/urls.py
+++ watchmate/watchmate/urls.py
@@ -6,5 +6,6 @@ import watchlist_app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('watch/', include('watchlist_app.api.urls'))
+    path('watch/', include('watchlist_app.api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
